b60a1a9bb69dcaf4d583ca74cf15176ef2b7ccf9
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-28 17:52:52 -0400

    add ssh option capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-
+set :ssh_options, { user: "deploy", forward_agent: true, auth_methods: %w(publickey) }
 server "104.248.116.231", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
