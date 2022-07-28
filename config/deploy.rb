1853067035d26bdfbb296ab88722dab623e6b0fa
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-28 18:59:33 -0400

    fix format

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock '~> 3.17.0'
 
-set :application, "scheduling"
-set :repo_url, "git@github.com:multinauta/scheduling.git"
+set :application, 'scheduling'
+set :repo_url, 'git@github.com:multinauta/scheduling.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -41,26 +41,26 @@ set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_error_log, "#{shared_path}/log/puma_error.log"
- 
-set :nginx_sites_available_path, "/etc/nginx/sites-available"
-set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
+
+set :nginx_sites_available_path, '/etc/nginx/sites-available'
+set :nginx_sites_enabled_path, '/etc/nginx/sites-enabled'
 
 namespace :puma do
-    desc 'Create Puma dirs'
-    task :create_dirs do
-      on roles(:app) do
-        execute "mkdir #{shared_path}/tmp/sockets -p"
-        execute "mkdir #{shared_path}/tmp/pids -p"
-      end
+  desc 'Create Puma dirs'
+  task :create_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
-   
-    desc "Restart Nginx"
-    task :nginx_restart do
-      on roles(:app) do
-        execute "sudo service nginx restart"
-      end
-    end
-   
-    before :start, :create_dirs
-    after :start, :nginx_restart
   end
+
+  desc 'Restart Nginx'
+  task :nginx_restart do
+    on roles(:app) do
+      execute 'sudo service nginx restart'
+    end
+  end
+
+  before :start, :create_dirs
+  after :start, :nginx_restart
+end
