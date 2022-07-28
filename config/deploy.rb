ed5633b77619ded08401db64f4f807ae851571a5
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-28 18:56:59 -0400

    remove puma config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.0'
+lock "~> 3.17.0"
 
-set :application, 'scheduling'
-set :repo_url, 'git@github.com:multinauta/scheduling.git'
+set :application, "scheduling"
+set :repo_url, "git@github.com:multinauta/scheduling.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -37,39 +37,3 @@ set :repo_url, 'git@github.com:multinauta/scheduling.git'
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-append :linked_dirs, 'log', 'tmp'
-
-set :keep_releases, 5
-set :migration_role, :app
-
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_access_log, "#{shared_path}/log/puma_access.log"
-set :puma_error_log, "#{shared_path}/log/puma_error.log"
-
-set :nginx_sites_available_path, '/etc/nginx/sites-available'
-set :nginx_sites_enabled_path, '/etc/nginx/sites-enabled'
-
-# Change ruby version
-set :rvm_type, :user
-set :rvm_ruby_version, '2.X.X'
-
-namespace :puma do
-  desc 'Create Puma dirs'
-  task :create_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  desc 'Restart Nginx'
-  task :nginx_restart do
-    on roles(:app) do
-      execute 'sudo service nginx restart'
-    end
-  end
-
-  before :start, :create_dirs
-  after :start, :nginx_restart
-end
