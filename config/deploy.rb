53d9ba7cb1634785cef6256bd5cbc0113695132b
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-28 18:58:32 -0400

    set deploy puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,30 @@ set :repo_url, "git@github.com:multinauta/scheduling.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+ 
+set :nginx_sites_available_path, "/etc/nginx/sites-available"
+set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
+
+namespace :puma do
+    desc 'Create Puma dirs'
+    task :create_dirs do
+      on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+      end
+    end
+   
+    desc "Restart Nginx"
+    task :nginx_restart do
+      on roles(:app) do
+        execute "sudo service nginx restart"
+      end
+    end
+   
+    before :start, :create_dirs
+    after :start, :nginx_restart
+  end
