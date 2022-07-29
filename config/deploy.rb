9b26f9fa98be5c5de45587a03a7644821d3f7600
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-28 23:22:03 -0400

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,37 +42,33 @@ set :pty, true
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-namespace :puma do
-    desc 'Make Directories for Puma Pids and Socket'
-    task :make_dirs do
-      on roles(:app) do
-        execute "mkdir #{shared_path}/tmp/sockets -p"
-        execute "mkdir #{shared_path}/tmp/pids -p"
-        execute "mkdir #{shared_path}/tmp/nginx -p"
-        execute "mkdir #{shared_path}/tmp/nginx/cache -p"
-      end
-    end
-  
-    before :start, :make_dirs
-  end
-  
-  namespace :deploy do
-    desc 'Initial Deploy'
-    task :initial do
-      on roles(:app) do
-        before 'deploy:restart', 'puma:start'
-        invoke 'deploy'
-      end
-    end
-  
-    desc 'Restart application'
-    task :restart do
-      on roles(:app), in: :sequence, wait: 5 do
-        invoke 'puma:restart'
-      end
-    end
+set :keep_releases, 5
+set :migration_role, :app
 
-    before :start, :make_dirs
-    after :start, :restart
-    after :finishing, :cleanup
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+
+set :nginx_sites_available_path, '/etc/nginx/sites-available'
+set :nginx_sites_enabled_path, '/etc/nginx/sites-enabled'
+
+namespace :puma do
+  desc 'Create Puma dirs'
+  task :create_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
   end
+
+  desc 'Restart Nginx'
+  task :nginx_restart do
+    on roles(:app) do
+      execute 'sudo service nginx restart'
+    end
+  end
+
+  before :start, :create_dirs
+  after :start, :nginx_restart
+end
