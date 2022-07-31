5bb94922b671f96215ddc8ed320c2ca2f47b0762
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-30 21:20:16 -0400

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,80 +1,74 @@
+
+######
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.0'
+lock "~> 3.17.0"
 
-set :application, 'scheduling'
-set :repo_url, 'git@github.com:multinauta/scheduling.git'
-set :rake, 'bundle exec rake'
-set :console_user, nil
-set :console_role, :app
-set :keep_releases, 5
+server '104.248.116.231', port: 22, roles: [:web, :app, :db], primary: true
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :repo_url,        'git@github.com:multinauta/scheduling.git'
+set :application,     'scheduling'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# If using Digital Ocean's Ruby on Rails Marketplace framework, your username is 'rails'
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-set :format, :airbrussh
-set :pty, true
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+#set :ssh_options, { user: "deploy", forward_agent: true, auth_methods: %w(publickey) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
+## Defaults:
+# set :scm,           :git
+set :branch,        :main
+# set :format,        :pretty
+# set :log_level,     :debug
 # set :keep_releases, 5
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-set :keep_releases, 5
-set :migration_role, :app
-
-
-append :linked_dirs, "log", "tmp", "pids", "sockets"
-
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log, "#{release_path}/log/puma.access.log"
-
-set :nginx_sites_available_path, '/etc/nginx/sites-available'
-set :nginx_sites_enabled_path, '/etc/nginx/sites-enabled'
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
-  desc 'Create Puma dirs'
-  task :create_dirs do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
     on roles(:app) do
       execute "mkdir #{shared_path}/tmp/sockets -p"
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
 
-  desc 'Restart Nginx'
-  task :nginx_restart do
+  before 'deploy:starting', 'puma:make_dirs'
+end
+
+namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
     on roles(:app) do
-      execute 'sudo service nginx restart'
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
     end
   end
 
-  after  :finishing, :compile_assets
-  before :start, :create_dirs
-  after :start, :nginx_restart
+  desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+      end
+  end
+
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
 end
