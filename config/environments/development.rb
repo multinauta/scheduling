d4618f29914735c05da72fa9ec0e0989453a2fbb
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-01 12:51:41 -0400

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => 'tmfjecmnmdzswulw',
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
