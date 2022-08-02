a8aaf9ddb23a0cbf81be85e03234776e4a6f8c70
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-01 21:38:35 -0400

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  #user.avatar.attach(user_params[:avatar])
+  user.avatar.attach(user_params[:avatar])
   def users_by_place
     @users = User.where(place_id: params[:place_id]).where.not(rol_id: [1]) 
     render json: @users
